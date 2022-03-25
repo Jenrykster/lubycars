b466a3ecc33597b17995d1ddd02de039b69c2494
João Henrique <joaoenrique.san@gmail.com>
2022-03-25 10:28:12 -0300

    feat: setup react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { MainPage } from './pages';
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/*' element={<h1>404 Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
