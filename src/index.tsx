c0019a5727acaa2288223565a3713a6cec16dd2c
João Henrique <joaoenrique.san@gmail.com>
2022-03-30 16:14:02 -0300

    feat: make the app deployable and change the README.md

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import { Header } from '@components';
 import { CarDetail, MainPage } from '@pages';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/car/:id' element={<CarDetail />} />
         <Route path='/*' element={<h1>404 Not found</h1>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
